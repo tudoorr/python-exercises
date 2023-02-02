518358869ce5050b38c9162c372ebe13ede60201
tudor <114509299+tudoorr@users.noreply.github.com>
2023-02-02 20:46:31 +0200

    Update timed_alphabet.py

--- timed_alphabet.py
+++ timed_alphabet.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from math import floor
 import string
 
 input("Press enter when you want to start entering the alphabet, the timer will also begin")
@@ -14,7 +15,7 @@ if alphabet.lower() == string.ascii_lowercase:
     if seconds_passed < 60:
         print(f"You took {seconds_passed} seconds to enter the alphabet")
     else:
-        minutes_passed = seconds_passed % 60
+        minutes_passed = floor(seconds_passed / 60)
         seconds_passed = seconds_passed - minutes_passed * 60
         print(f"\nYou took {minutes_passed} minutes and {seconds_passed} seconds to enter the alphabet\n")
 else:
