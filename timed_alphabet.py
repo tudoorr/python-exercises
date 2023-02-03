127447c21b970c8e2e2f5c33b67bc3dd15cc8fec
tudor <114509299+tudoorr@users.noreply.github.com>
2023-02-03 19:49:16 +0200

    Update timed_alphabet.py

--- timed_alphabet.py
+++ timed_alphabet.py
@@ -1,22 +1,19 @@
 from datetime import datetime
-from math import floor
 import string
 
 input("Press enter when you want to start entering the alphabet, the timer will also begin")
-start_second = datetime.now().strftime("%S")
+start_time = datetime.now().strftime("%M:%S")
+start_minute, start_second = start_time.split(":")
 alphabet = input(">")
+end_time = datetime.now().strftime("%M:%S")
+end_minute, end_second = end_time.split(":")
 
 if alphabet.lower() == string.ascii_lowercase:
-    end_second = datetime.now().strftime("%S")
-    if end_second < start_second:
-        seconds_passed = int(end_second) + 60 - int(start_second)
+    minutes_passed = int(end_minute) - int(start_minute)
+    seconds_passed = int(end_second) + minutes_passed * 60 - int(start_second)
+    if minutes_passed == 0:
+        print(f"{seconds_passed} seconds have passed, nice!)
     else:
-        seconds_passed = int(end_second) - int(start_second)
-    if seconds_passed < 60:
-        print(f"You took {seconds_passed} seconds to enter the alphabet")
-    else:
-        minutes_passed = floor(seconds_passed / 60)
-        seconds_passed = seconds_passed - minutes_passed * 60
-        print(f"\nYou took {minutes_passed} minutes and {seconds_passed} seconds to enter the alphabet\n")
+        print(f"\n{minutes_passed} minutes and {seconds_passed} seconds took you to enter the alphabet, you're slow!)
 else:
     print("You did not enter the alphabet correctly!\n")
