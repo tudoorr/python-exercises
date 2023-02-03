dd4689a26d077e58148211226910439a01542f1b
tudor <114509299+tudoorr@users.noreply.github.com>
2023-02-03 19:40:59 +0200

    Update timer_guess.py

--- timer_guess.py
+++ timer_guess.py
@@ -1,25 +1,23 @@
 from datetime import datetime
-from math import floor
+from time import sleep
 
-input("Press enter to start and the timer will begin")
+input("Press enter to start and the timer will begin in 3 seconds")
+sleep(3)
 
-start_second = datetime.now().strftime("%S")
+start_time = datetime.now().strftime("%M:%S")
+start_minute, start_second = start_time.split(":")
 
 input("\nTimer started, press enter when you think 10 seconds had passed\n")
 
-end_second = datetime.now().strftime("%S")
+end_time = datetime.now().strftime("%M:%S")
+end_minute, end_second = end_time.split(":")
 
-if end_second < start_second:
-    seconds_passed = int(end_second) + minutes_passed * 60 - int(start_second)
-else:
-    seconds_passed = int(end_second) - int(start_second)
+minutes_passed = end_minute - start_minute
+seconds_passed = seconds_passed - minutes_passed * 60
 
-if seconds_passed < 60:
+if int(minutes_passed) == 0:
     print(f"{seconds_passed} seconds have passed")
+    if int(seconds_passed) == 10:
+        print("\nYou did it perfectly!\n")
 else:
-    minutes_passed = floor(seconds_passed / 60)
-    seconds_passed = seconds_passed - minutes_passed * 60
     print(f"\n{minutes_passed} minutes and {seconds_passed} seconds have passed\n")
-
-if seconds_passed == 10 and minutes_passed == 0:
-    print("\nYou did it perfectly!\n")
