95b1d315cffeec83f4d44de83c6dd787f208df8b
tudor <114509299+tudoorr@users.noreply.github.com>
2023-02-11 13:58:05 +0200

    Update x_and_o.py

--- x_and_o.py
+++ x_and_o.py
@@ -51,7 +51,7 @@ print_game_board()
 while True:
     while True:
         try:
-            player("X")
+            player(sign_player)
             break
         except IndexError:
             print("\nYou did not respect the format or you entered an out of bounds spot!\n")
