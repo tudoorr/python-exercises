4c3a9c4fc7ebffec8a1491a4bd398d7a3df93276
tudor <114509299+tudoorr@users.noreply.github.com>
2023-02-24 16:41:25 +0200

    Create caesar_cipher.py

---
+++ caesar_cipher.py
@@ -0,0 +1,26 @@
+import string
+
+word = input("What word do you want to encode?: ")
+try:
+    shift = input("How many places do you want to shift it? (non negative): ")
+    if "-" in shift:
+        print("It cannot be a negative number!")
+        exit(0)
+    shift = int(shift)
+except TypeError:
+    print("It needs to be a number!")
+    exit(0)
+result = ""
+
+for char in word:
+    if char == " ":
+        result += " "
+    elif char.islower():
+        result += string.ascii_lowercase[(list(string.ascii_lowercase).index(char) + shift) % 26]
+    elif char.isupper():
+        result += string.ascii_uppercase[(list(string.ascii_uppercase).index(char) + shift) % 26]
+    else:
+        print("You can't have special characters!")
+        exit(0)
+
+print(result)
