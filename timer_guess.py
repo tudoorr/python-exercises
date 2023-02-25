b3f2b2d3db8606888d48295047383e417d212248
tudor <114509299+tudoorr@users.noreply.github.com>
2023-02-25 12:56:04 +0200

    Update timer_guess.py

--- timer_guess.py
+++ timer_guess.py
@@ -3,14 +3,9 @@ from time import sleep
 
 input("Press enter to start and the timer will begin in 3 seconds")
 sleep(3)
-
-start_time = datetime.now().strftime("%M:%S")
-start_minute, start_second = start_time.split(":")
-
+start_minute, start_second = datetime.now().strftime("%M:%S").split(":")
 input("\nTimer started, press enter when you think 10 seconds had passed\n")
-
-end_time = datetime.now().strftime("%M:%S")
-end_minute, end_second = end_time.split(":")
+end_minute, end_second = datetime.now().strftime("%M:%S").split(":")
 
 minutes_passed = int(end_minute) - int(start_minute)
 seconds_passed = int(end_second) + minutes_passed * 60 - int(start_second)
